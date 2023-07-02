b8cbd03b40fe2d3c9f63b2fe91b180350b60513d
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-07-02 16:36:10 +0200

    upd3

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const contactsInitialState = [];
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.push(action.payload);
+      }
+    },
+    deleteContact(state, action) {
+      const index = state.findIndex(contact => contact.name === action.payload);
+      state.splice(index, 1);
+    }
+  }
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+
+export const contactsReducer = contactsSlice.reducer;
