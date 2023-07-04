b0e030ec00e44f34205dd43209a359f864e27f41
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-07-04 15:10:13 +0200

    upd11

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,8 +14,7 @@ import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
   key: 'root',
-  storage,
-  whitelist: ['contacts'],
+  storage
 };
 
 const persistedReducer = persistReducer(persistConfig, contactsReducer);
