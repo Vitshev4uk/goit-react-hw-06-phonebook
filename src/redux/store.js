59af20c70eab918d4ad66d7fbe7425e36f366b67
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-07-04 14:42:15 +0200

    upd9

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './contactsSlice';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import { configureStore } from '@reduxjs/toolkit';
+// import { contactsReducer } from './contactsSlice';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['contacts'],
-};
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   whitelist: ['contacts'],
+// };
 
-const persistedReducer = persistReducer(persistConfig, contactsReducer);
+// const persistedReducer = persistReducer(persistConfig, contactsReducer);
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: persistedReducer,
+//   },
+// });
+
+// export const persistor = persistStore(store);
+
+
+import { configureStore } from "@reduxjs/toolkit";
+import { contactsReducer } from "./contactsSlice";
 
 export const store = configureStore({
-  reducer: {
-    contacts: persistedReducer,
-  },
-});
-
-export const persistor = persistStore(store);
+    reducer: {
+        contacts: contactsReducer
+    }
+})
